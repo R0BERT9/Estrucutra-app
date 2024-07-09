8cf64a309f257fdce19338cfec68c6c2550488a1
Axelhdzgithup <axel.hernandez@uttn.mx>
2024-07-09 13:48:48 -0500

    INTENTO DE LOGIN

--- www/Public/js/firebaseconfig.js
+++ www/Public/js/firebaseconfig.js
@@ -1,11 +1,20 @@
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  };
+
+  apiKey: "AIzaSyDoS_M_cDxN_kJ-vX5jfG12gnPnfFa0Cq4",
+
+  authDomain: "myhealthpal-4b2d6.firebaseapp.com",
+
+  databaseURL: "https://myhealthpal-4b2d6-default-rtdb.firebaseio.com",
+
+  projectId: "myhealthpal-4b2d6",
+
+  storageBucket: "myhealthpal-4b2d6.appspot.com",
+
+  messagingSenderId: "346732083941",
+
+  appId: "1:346732083941:web:958b3b6d1d7067e71b720c"
+
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
